4c0d7b1c797a59a75d8344990af1b4fc6798f987
Emre Yurtseven <yurtseven.education@gmail.com>
2022-10-24 15:34:06 +0300

    Updated the function of capitaliceSentence

--- app/src/main/java/com/emreyurtseven/kotlinchallenges/capitalizeSentence.kt
+++ app/src/main/java/com/emreyurtseven/kotlinchallenges/capitalizeSentence.kt
@@ -1,13 +1,22 @@
 package com.emreyurtseven.kotlinchallenges
 
 fun main(){
-    capitalizeSentence()
+    capitalizeSentence("function that capitalizes the initials of the words in this sentence")
+    capitalizeWordList()
 }
-private fun capitalizeSentence(){
+private fun capitalizeWordList(){
     val list = arrayListOf<String>("emre", "özge", "pınar", "meral", "halil", "şeyma", "elif")
 
     list.forEach {
         println(it.replaceFirstChar { it.uppercase() })
     }
+
+}
+
+private fun capitalizeSentence(word: String){
+
+    println(word.split(" ").joinToString(" ") {
+        it.replaceFirstChar { it.uppercase() }
+    })
 
 }
